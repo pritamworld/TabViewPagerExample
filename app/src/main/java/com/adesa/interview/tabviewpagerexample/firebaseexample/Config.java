397521e1f1399dfdc4ad2862e7759fba11e6a0b2
pritesh.patel <pritam.world@gmail.com>
2016-09-07 14:56:00 -0400

    FCM Implementation

---
+++ app/src/main/java/com/adesa/interview/tabviewpagerexample/firebaseexample/Config.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016. Pritesh Patel
+ */
+
+package com.adesa.interview.tabviewpagerexample.firebaseexample;
+
+/**
+ * Created by pritesh.patel on 2016-09-07, 1:17 PM.
+ * ADESA, Canada
+ */
+public class Config {
+    public static final String FIREBASE_URL = "https://first-project-93ee6.firebaseio.com/";
+}
---
+++ app/src/main/java/com/adesa/interview/tabviewpagerexample/firebaseexample/Person.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016. Pritesh Patel
+ */
+
+package com.adesa.interview.tabviewpagerexample.firebaseexample;
+
+/**
+ * Created by pritesh.patel on 2016-09-07, 1:20 PM.
+ * ADESA, Canada
+ */
+public class Person {
+    private String name;
+    private String address;
+
+    public Person() {
+      /*Blank default constructor essential for Firebase*/
+    }
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
