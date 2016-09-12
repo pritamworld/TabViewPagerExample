7c99653b91970921b7c38661bb117b96f07f54af
pritesh.patel <pritam.world@gmail.com>
2016-09-12 11:52:38 -0400

    Marquee Textview test

--- app/src/main/java/com/adesa/interview/tabviewpagerexample/TirangleBadgeViewActivity.java
+++ app/src/main/java/com/adesa/interview/tabviewpagerexample/TirangleBadgeViewActivity.java
@@ -6,12 +6,23 @@ package com.adesa.interview.tabviewpagerexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class TirangleBadgeViewActivity extends AppCompatActivity {
+
+    @InjectView(R.id.txtScrollingText)
+    TextView txtScrollingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tirangle_badge_view);
+        ButterKnife.inject(this);
+
+        txtScrollingText.setSelected(true);
+        //txtScrollingText.requestFocus();
     }
 }
