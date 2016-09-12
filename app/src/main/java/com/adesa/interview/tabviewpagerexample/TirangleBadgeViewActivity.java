ce54b947e0dd0b88ef70e2f4c262f54beb1f074f
pritesh.patel <pritam.world@gmail.com>
2016-09-12 10:10:14 -0400

    Triangle Labe lView addes

---
+++ app/src/main/java/com/adesa/interview/tabviewpagerexample/TirangleBadgeViewActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016. Pritesh Patel
+ */
+
+package com.adesa.interview.tabviewpagerexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TirangleBadgeViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tirangle_badge_view);
+    }
+}
