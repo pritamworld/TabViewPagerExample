50b63a8b40e5c626f415bd37f9eff1cff331c134
pritesh.patel <pritam.world@gmail.com>
2016-09-13 13:32:16 -0400

    Fragment added

---
+++ app/src/main/java/com/adesa/interview/tabviewpagerexample/fragmentexample/DetailsFragment.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2016. Pritesh Patel
+ */
+
+package com.adesa.interview.tabviewpagerexample.fragmentexample;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.adesa.interview.tabviewpagerexample.R;
+
+
+public class DetailsFragment extends Fragment {
+    public static String KEY_POSITION = "param";
+    TextView txtTitle;
+    private OnFragmentInteractionListener mListener;
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // During the startup, we check if there are any arguments passed to the fragment.
+        // onStart() is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call the method below
+        // that sets the description text
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set description based on argument passed in
+            setTextValue(args.getString(KEY_POSITION));
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public void setTextValue(String str) {
+        txtTitle.setText(str);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
