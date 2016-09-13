f4a721736511e3e5b92c3c1069c8d419eff34885
pritesh.patel <pritam.world@gmail.com>
2016-09-13 09:08:15 -0400

    Constraint layout added - Android Studio Preview 2.2

--- app/src/main/java/com/adesa/interview/tabviewpagerexample/TirangleBadgeViewActivity.java
+++ app/src/main/java/com/adesa/interview/tabviewpagerexample/TirangleBadgeViewActivity.java
@@ -6,15 +6,11 @@ package com.adesa.interview.tabviewpagerexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class TirangleBadgeViewActivity extends AppCompatActivity {
 
-    @InjectView(R.id.txtScrollingText)
-    TextView txtScrollingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +18,5 @@ public class TirangleBadgeViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tirangle_badge_view);
         ButterKnife.inject(this);
 
-        //txtScrollingText.setSelected(true);
-        //txtScrollingText.requestFocus();
-
-        /*txtScrollingText.setText("welcome_to_android_programming_welcome_to_ios_programming_welcome_to_blackberry_programming_welcome_to_net_programming_welcome_to_java_programming");
-        txtScrollingText.setEllipsize(TextUtils.TruncateAt.MARQUEE);
-        txtScrollingText.setSingleLine(true);
-        txtScrollingText.setMarqueeRepeatLimit(5);
-        txtScrollingText.setSelected(true);*/
     }
 }
