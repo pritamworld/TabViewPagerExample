ac8de8b6d5c527dfff2b0d2eebd166dd1f07fb2c
pritesh.patel <pritam.world@gmail.com>
2016-09-23 15:41:28 -0400

    WebView and JavaScript Example Added

--- app/src/main/java/com/moxdroid/interview/tabviewpagerexample/daggerexample/DaggerActivity.java
+++ app/src/main/java/com/moxdroid/interview/tabviewpagerexample/daggerexample/DaggerActivity.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.adesa.interview.tabviewpagerexample.R;
@@ -29,18 +30,25 @@ import com.moxdroid.interview.tabviewpagerexample.daggerexample.module.VehicleMo
 
 import java.util.List;
 
+//Sensor Example is in this file
 public class DaggerActivity extends AppCompatActivity implements SensorEventListener {
 
     private static String TAG = DaggerActivity.class.getName();
-    Vehicle vehicle;
+
     ActionBar actionBar;
+    Vehicle vehicle;
+    private ImageView mPointer;
+    private TextView valueView, directionView;
     private SensorManager mSensorManager;
     private Sensor mLight;
     private Sensor mAccelerometer;
     private Sensor mMagnetometer;
-    private ImageView mPointer;
+    private Sensor mProximity;
+    private Sensor mGravity;
+
     private float[] mLastAccelerometer = new float[3];
     private float[] mLastMagnetometer = new float[3];
+    private float[] mLastGravity = new float[3];
     private boolean mLastAccelerometerSet = false;
     private boolean mLastMagnetometerSet = false;
     private float[] mR = new float[9];
@@ -52,13 +60,14 @@ public class DaggerActivity extends AppCompatActivity implements SensorEventList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dagger);
 
-        mPointer = (ImageView) findViewById(R.id.pointer);
+        initView();
 
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
+        initDagger();
 
+        getSensor();
+    }
 
+    private void initDagger() {
         VehicleComponent component = DaggerVehicleComponent.builder().vehicleModule(new VehicleModule()).build();
 
         vehicle = component.provideVehicle();
@@ -68,8 +77,15 @@ public class DaggerActivity extends AppCompatActivity implements SensorEventList
         vehicle.increaseSpeed(10);
 
         Toast.makeText(this, String.valueOf(vehicle.getSpeed()), Toast.LENGTH_SHORT).show();
+    }
 
-        getSensor();
+    private void initView() {
+        mPointer = (ImageView) findViewById(R.id.pointer);
+        valueView = (TextView) findViewById(R.id.values);
+        directionView = (TextView) findViewById(R.id.direction);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
     }
 
     @Override
@@ -78,6 +94,8 @@ public class DaggerActivity extends AppCompatActivity implements SensorEventList
         mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
         mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -86,6 +104,8 @@ public class DaggerActivity extends AppCompatActivity implements SensorEventList
         mSensorManager.unregisterListener(this, mLight);
         mSensorManager.unregisterListener(this, mAccelerometer);
         mSensorManager.unregisterListener(this, mMagnetometer);
+        mSensorManager.unregisterListener(this, mProximity);
+        mSensorManager.unregisterListener(this, mGravity);
     }
 
     @Override
@@ -129,22 +149,23 @@ public class DaggerActivity extends AppCompatActivity implements SensorEventList
         mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-// The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
-        float lux = sensorEvent.values[0];
-        // Do something with this sensor value.
-        Log.d(TAG, "onSensorChanged: " + lux);
-
+        // The light sensor returns a single value.
 
         if (sensorEvent.sensor == mLight) {
             //Close Activity if lex <= 20
+            // Many sensors return 3 values, one for each axis.
+            float lux = sensorEvent.values[0];
+            // Do something with this sensor value.
+            //Log.d(TAG, "onSensorChanged: " + lux);
             actionBar.setTitle("Light : " + lux);
             if ((int) lux <= 20) {
-                finish();
+                //finish();
             }
         }
 
@@ -173,11 +194,74 @@ public class DaggerActivity extends AppCompatActivity implements SensorEventList
 
             mPointer.startAnimation(ra);
             mCurrentDegree = -azimuthInDegress;
+            updateDirection();
+        }
+
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            float prox = sensorEvent.values[0];
+            // Do something with this sensor value.
+            Log.d(TAG, "Proximity: " + prox);
+        }
+
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_GRAVITY) {
+            System.arraycopy(sensorEvent.values, 0, mLastGravity, 0, sensorEvent.values.length);
+            Log.d(TAG, "Gravity : " + mLastGravity.toString());
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    private void updateDirection() {
+        float[] temp = new float[9];
+        float[] R = new float[9];
+        //Load rotation matrix into R
+        SensorManager.getRotationMatrix(temp, null, mLastAccelerometer, mLastMagnetometer);
+        //Remap to camera's point-of-view
+        SensorManager.remapCoordinateSystem(temp, SensorManager.AXIS_X, SensorManager.AXIS_Z, R);
+        //Return the orientation values
+        float[] values = new float[3];
+        SensorManager.getOrientation(R, values);
+        //Convert to degrees
+        for (int i = 0; i < values.length; i++) {
+            Double degrees = (values[i] * 180) / Math.PI;
+            values[i] = degrees.floatValue();
+        }
+        //Display the compass direction
+        directionView.setText(getDirectionFromDegrees(values[0]));
+        //Display the raw values
+        valueView.setText(String.format("Azimuth: %1$1.2f, Pitch: %2$1.2f, Roll: %3$1.2f",
+                values[0], values[1], values[2]));
+    }
+
+    private String getDirectionFromDegrees(float degrees) {
+        if (degrees >= -22.5 && degrees < 22.5) {
+            return "N";
+        }
+        if (degrees >= 22.5 && degrees < 67.5) {
+            return "NE";
+        }
+        if (degrees >= 67.5 && degrees < 112.5) {
+            return "E";
+        }
+        if (degrees >= 112.5 && degrees < 157.5) {
+            return "SE";
+        }
+        if (degrees >= 157.5 || degrees < -157.5) {
+            return "S";
+        }
+        if (degrees >= -157.5 && degrees < -112.5) {
+            return "SW";
+        }
+        if (degrees >= -112.5 && degrees < -67.5) {
+            return "W";
+        }
+        if (degrees >= -67.5 && degrees < -22.5) {
+            return "NW";
+        }
+
+        return null;
     }
 }
