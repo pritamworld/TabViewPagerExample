3a94767c8ce0ca31a6d5976177e83963df752533
pritesh.patel <pritam.world@gmail.com>
2016-09-23 08:36:10 -0400

    Sensor Example Added

--- app/src/main/java/com/moxdroid/interview/tabviewpagerexample/daggerexample/DaggerActivity.java
+++ app/src/main/java/com/moxdroid/interview/tabviewpagerexample/daggerexample/DaggerActivity.java
@@ -4,8 +4,21 @@
 
 package com.moxdroid.interview.tabviewpagerexample.daggerexample;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.adesa.interview.tabviewpagerexample.R;
@@ -14,14 +27,36 @@ import com.moxdroid.interview.tabviewpagerexample.daggerexample.component.Vehicl
 import com.moxdroid.interview.tabviewpagerexample.daggerexample.model.Vehicle;
 import com.moxdroid.interview.tabviewpagerexample.daggerexample.module.VehicleModule;
 
-public class DaggerActivity extends AppCompatActivity {
+import java.util.List;
 
+public class DaggerActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static String TAG = DaggerActivity.class.getName();
     Vehicle vehicle;
+    ActionBar actionBar;
+    private SensorManager mSensorManager;
+    private Sensor mLight;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private ImageView mPointer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dagger);
+
+        mPointer = (ImageView) findViewById(R.id.pointer);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
 
 
         VehicleComponent component = DaggerVehicleComponent.builder().vehicleModule(new VehicleModule()).build();
@@ -33,5 +68,116 @@ public class DaggerActivity extends AppCompatActivity {
         vehicle.increaseSpeed(10);
 
         Toast.makeText(this, String.valueOf(vehicle.getSpeed()), Toast.LENGTH_SHORT).show();
+
+        getSensor();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this, mLight);
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_refresh:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+
+
+    private void getSensor() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (Sensor sensor : deviceSensors) {
+            Log.d(TAG, sensor.getName() + " [ " + sensor.getVendor() + " ]");
+        }
+
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+// The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float lux = sensorEvent.values[0];
+        // Do something with this sensor value.
+        Log.d(TAG, "onSensorChanged: " + lux);
+
+
+        if (sensorEvent.sensor == mLight) {
+            //Close Activity if lex <= 20
+            actionBar.setTitle("Light : " + lux);
+            if ((int) lux <= 20) {
+                finish();
+            }
+        }
+
+        if (sensorEvent.sensor == mAccelerometer) {
+            System.arraycopy(sensorEvent.values, 0, mLastAccelerometer, 0, sensorEvent.values.length);
+            mLastAccelerometerSet = true;
+        } else if (sensorEvent.sensor == mMagnetometer) {
+            System.arraycopy(sensorEvent.values, 0, mLastMagnetometer, 0, sensorEvent.values.length);
+            mLastMagnetometerSet = true;
+        }
+
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+            float azimuthInDegress = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+            RotateAnimation ra = new RotateAnimation(
+                    mCurrentDegree,
+                    -azimuthInDegress,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
+
+            ra.setDuration(250);
+            ra.setFillAfter(true);
+
+            mPointer.startAnimation(ra);
+            mCurrentDegree = -azimuthInDegress;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
